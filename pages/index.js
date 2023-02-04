5fbd750973ad57abc98b9a05fbb7d1b72ebeb1a1
alfitof <alfito.fbriansyah@gmail.com>
2023-02-05 02:08:01 +0700

    Fix bug

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,16 @@
 import Head from 'next/head';
 import { HiOutlineViewGridAdd } from 'react-icons/hi';
-import Table from '@/components/Table';
-import Form from '@/components/Form';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
-const Manage = dynamic(() => import('../container/Manage'));
-const Navigation = dynamic(() => import('../components/Navigation'));
+import Navigation from '../components/navigation';
 
 export default function Home() {
   return (
     <div>
       <Navigation />
+      Navigation
       {/* <Manage /> */}
     </div>
   );
